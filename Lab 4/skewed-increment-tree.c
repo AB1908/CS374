4b188158676e1137981f480caecee4d487a287b1
AB1908 <abhishekbhattacherjee@live.com>
2018-08-16 10:04:21 +0530

    Add skewed process tree with increment implementation

---
+++ Lab 4/skewed-increment-tree.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/unistd.h>
+
+int main()
+{
+    int i=0,n;
+    int fd[2];
+    int pipe_status = pipe(fd);
+
+    printf("\nEnter a value for n: ");
+    scanf("%d",&n);
+    
+    int fork_status = fork();
+
+    for(i = 0; i < n-1; i++)
+    {
+        if(fork_status > 0)
+        {
+            if (!i)
+            {
+                int incr;
+                printf("\nEnter a value to increment:");
+                scanf("%d",&incr);
+                close(fd[0]);
+                write(fd[1], &incr, sizeof(int));
+            }
+            wait(0);
+            break;
+        }
+        else if(fork_status == 0)
+        {
+            int incr;
+            read(fd[0], &incr, sizeof(int));
+            incr++;
+            write(fd[1], &incr, sizeof(int));
+            printf("\n%d has created a child process %d.\n", (int)getppid(), (int)getpid());
+            fork_status = fork();
+        }
+        else
+        {
+            printf("\nChild process creation unsuccesful.");
+        }
+        if(i == n-2)
+        {
+            int incr;
+            read(fd[0], &incr, sizeof(int));
+            printf("\nThe final incremented value is %d\n", incr);
+        }
+    }
+    return 0;
+}
