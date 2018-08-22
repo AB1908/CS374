57c675c11fb030ae7eecab850d737eba747adfec
AB1908 <abhishekbhattacherjee@live.com>
2018-08-22 21:39:49 +0530

    Move implementation to main()
    The original functional implementation did not work when reading from
    the pipe, so the implementation was changed to use main().

--- Lab 4/fork-and-pipe.c
+++ Lab 4/fork-and-pipe.c
@@ -5,37 +5,6 @@
 #include <wait.h>
 #include <sys/types.h>
 
-int child(int child_count, int *fd)
-{
-    if(child_count == 1)
-    {
-        // close(0);
-        close(fd[0]);
-        close(1);
-        // dup2(1, fd[1]);
-        dup(fd[1]);
-        // char args[] = {"ls", "\0"};
-        int exec_status = execl("/bin/ls", "ls", "-l", "/home/abhishek");
-        // printf("%d", exec_status);
-    }
-    else if(child_count == 2)
-    {
-        // printf("Hello Darkness my old friend");
-        // close(0);
-        // dup(fd[0]);
-        char buf[1000];
-        // close(fd[1]);
-        int rd = read(fd[0], buf, 1000);
-        // close(fd[0]);
-        // dup2(fd[1],1);
-        // char *args[] = {"wc", buf, "\0"};
-        // int exec_status = execv("/bin/wc", args);
-        int exec_status = execl("/bin/wc", "wc", buf);
-        printf("%d",exec_status);
-    }
-    return 0;
-}
-
 int main()
 {
     int child_count = -1;
@@ -51,30 +20,36 @@ int main()
             printf("\nChild creation unsuccessful");
             break;
         }
-        else if(fork_status == 0)
-        {
-            printf("\nProcess %d was spawned by process %d\n", (int)getpid(), (int)getppid());
-            break;
-        }
         else
         {
             fork_status = (int)fork();
             child_count = i;
-            wait(0);
+            if (fork_status == 0) // Don't fork any more if child
+            {
+                printf("\nProcess %d was spawned by process %d\n", (int)getpid(), (int)getppid());
+                break;
+            }
         }
     }
 
     if(fork_status != 0)
     {
-        // printf("Man");
-        // wait(0);
+        wait(0);
+        close(fd[0]); // Close the remaining pipe end once children are dead
     }
     else
     {
-        int proc_status = child(child_count, fd);
-        // if (!child_count)
+        if(child_count == 1) // If first child, redirect STDOUT to pipe and execute ls
         {
-            // int exec_status = execl("/bin/ls", "ls", "-l", "/home/abhishek");           
+            close(fd[0]);
+            dup2(fd[1], 1);
+            int exec_status = execl("/bin/ls", "ls", "-l", "/home/abhishek", (char *)NULL);
+        }
+        if(child_count == 2) // If second child, redirect pipe write end to STDIN for "wc" to use as input
+        {
+            close(fd[1]);
+            int dup_status = dup2(fd[0], 0);
+            int exec_status = execl("/bin/wc", "wc", (char *)NULL);
         }
     }
     return 0;
