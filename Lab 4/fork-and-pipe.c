bc9284f2a2f899561d785f396c3302bcb8c919e3
AB1908 <abhishekbhattacherjee@live.com>
2018-08-22 18:48:43 +0530

    Redirect first process output to pipe

--- Lab 4/fork-and-pipe.c
+++ Lab 4/fork-and-pipe.c
@@ -7,37 +7,44 @@
 
 int child(int child_count, int *fd)
 {
-    if(child_count == 0)
+    if(child_count == 1)
     {
-        // close(fd[0]);
-        // close(fd[1]);
-        // dup2(1,fd[1]);
+        // close(0);
+        close(fd[0]);
+        close(1);
+        // dup2(1, fd[1]);
+        dup(fd[1]);
         // char args[] = {"ls", "\0"};
-        int exec_status = execl("ls", "\0");
+        int exec_status = execl("/bin/ls", "ls", "-l", "/home/abhishek");
         // printf("%d", exec_status);
     }
-    else
+    else if(child_count == 2)
     {
         // printf("Hello Darkness my old friend");
-        char buf[100];
-        close(fd[1]);
-        int rd = read(fd[0], buf, 100);
+        // close(0);
+        // dup(fd[0]);
+        char buf[1000];
+        // close(fd[1]);
+        int rd = read(fd[0], buf, 1000);
         // close(fd[0]);
         // dup2(fd[1],1);
-        char *args[] = {"wc", buf, "\0"};
-        int exec_status = execv("/bin/wc", args);
+        // char *args[] = {"wc", buf, "\0"};
+        // int exec_status = execv("/bin/wc", args);
+        int exec_status = execl("/bin/wc", "wc", buf);
+        printf("%d",exec_status);
     }
     return 0;
 }
 
 int main()
 {
-    int child_count, i;
+    int child_count = -1;
+    int i;
     int fd[2];
     int pipe_status = pipe(fd);
     int fork_status = 1;
 
-    for(i = 0; i < 2; i++)
+    for(i = 0; i < 3; i++)
     {
         if(fork_status < 0)
         {
@@ -47,22 +54,28 @@ int main()
         else if(fork_status == 0)
         {
             printf("\nProcess %d was spawned by process %d\n", (int)getpid(), (int)getppid());
+            break;
         }
         else
         {
-            fork_status = fork();
+            fork_status = (int)fork();
             child_count = i;
-            // wait(0);
+            wait(0);
         }
     }
 
-    if(fork_status == 0)
+    if(fork_status != 0)
     {
-        int proc_status = child(child_count, fd);
+        // printf("Man");
+        // wait(0);
     }
     else
     {
-        wait(0);
+        int proc_status = child(child_count, fd);
+        // if (!child_count)
+        {
+            // int exec_status = execl("/bin/ls", "ls", "-l", "/home/abhishek");           
+        }
     }
     return 0;
 }
