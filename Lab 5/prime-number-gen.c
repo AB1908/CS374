83e05e598673f88994c23de9b2683b4a0a9c942a
AB1908 <abhishekbhattacherjee@live.com>
2018-08-25 17:14:58 +0530

    Refactor pipe writing cases for clarity
    - Linux Kernel Style comments were added for clarity.
    - The cases for writing to the pipe were refactored to be more logically
    appropriate.
    - The need for the extra pipe was removed.
    - Variable names were changed for clarity.
    - The correct output was accounted for by changing the array size and
    refactoring as necessary.
    - Add close() calls to the pipes which had been left opened.

--- Lab 5/prime-number-gen.c
+++ Lab 5/prime-number-gen.c
@@ -1,4 +1,7 @@
-/* Sieve of Eratosthenes*/
+/**
+ * Sieve of Eratosthenes program where filtering is done by a child process.
+ * NOTE: Compilation must be done with the `-lm` flag as math library is not properly linked in gcc.
+ */
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -18,29 +21,51 @@ void print_array(int *arr, int length)
     printf("\n");
 }
 
-// This method filters out the non-prime numbers and reallocs the array to the smaller size required.
-int sieve(int *arr, int original_len, int child_no)
+/**
+ * sieve() - Filters out the non-prime numbers and reallocs the array to a smaller size.
+ * @*arr:           Pointer to the array to be filtered
+ * @original_len:   The length of the array
+ * @filter_val:     The number whose multiples are to be filtered out
+ * 
+ * The sieve() method takes the original array as input and then filters out the numbers
+ * which are multiples of filter_no. final_length keeps track of the index where the
+ * acceptable numbers are copied to. The condition essentially skips copying any number
+ * which is a multiple of filter_no.
+ * 
+ * Return:          The new length of the array
+ */
+int sieve(int *arr, int original_len, int filter_val)
 {
     int i, final_length = 0;
 
     for(i = 0; i < original_len; i++)
     {
-        if((arr[i] % child_no != 0) || (arr[i] == child_no))
+        if((arr[i] % filter_val != 0) || (arr[i] == filter_val))
         {
             arr[final_length++] = arr[i];
         }
     }
 
-    arr = (int*)realloc(arr, (final_length)*sizeof(int));
+    arr = (int*)realloc(arr, final_length*sizeof(int));
         
     return final_length;
 }
 
-// This method prints the array before and after filtering
+/**
+ * filter_print() - Print the array before and after applying sieve
+ * @*arr:       Pointer to array
+ * @n:          Length of array
+ * @child_no:   Child process number, which is used for filtering
+ * 
+ * child_no is the parameter to sieve() as the first child filters out
+ * multiples of two, second child filters out multiples of three and so on.
+ * 
+ * Return: New (smaller) array size
+ */
 int filter_print(int *arr, int n, int child_no)
 {
     print_array(arr, n);
-    n = sieve(arr, n, child_no+1);
+    n = sieve(arr, n, child_no);
     print_array(arr, n);
     return n;
 }
@@ -52,22 +77,13 @@ int main()
 
     printf("\nEnter a perfect square:");
     scanf("%d", &n);
-
-    int *arr = (int*)malloc(sizeof(int)*n); // Arrays created on the stack cannot be reallocated
+    
+    int arr_size = n - 1;
+    int *arr = (int*)malloc(sizeof(int)*arr_size); // Arrays created on the stack cannot be reallocated
     int i, j;
     int children = (int)sqrt(n);
     int fork_status = 0;
-
-    // Initialise the original array
-    for(i = 1; i <= n; i++)
-    {
-        arr[i-1] = i;
-    }
-
-    // Since the implementation starts with a read as below, the initial array is written to a pipe. A better implementation could be possible.
     int pipe_status = pipe(fd);
-    int wr = write(fd[1], &arr, sizeof(int)*n);
-    close(fd[1]);
 
     for(i = 1; i < children; i++)
     {
@@ -75,37 +91,67 @@ int main()
         {
             break;
         }
-        else if(fork_status == -1)
+        /**
+         * For the original process, generate the array and write it to a pipe
+         */
+        else if((fork_status == 0) && (i == 1))
         {
-            printf("\nChild creation unsuccessful\n");
-        }
-        else // Child block
-        {
-            int rd = read(fd[0], &arr, sizeof(arr));
+            for(j = 0; j < arr_size; j++)
+            {
+                arr[j] = j + 2;
+            }
             
-            printf("\nThe process %d was spawned by parent process %d\n", (int)getpid(), (int)getppid());
-            n = filter_print(arr, n, i);
-            
-            // Create a new pipe which is accessible from the child which the (then) parent writes to for the child to read in the next iteration
-            close(fd[0]);
-            pipe_status = pipe(fd);          
             fork_status = fork();
             if(fork_status > 0)
             {
+                close(fd[0]);                
                 write(fd[1], &arr, sizeof(arr));
                 close(fd[1]);
                 wait(0);
             }
         }
+        /**
+        * For a child process, close the file descriptors of the existing pipe
+        * after reading and apply the filter. Then, create a new pipe which is
+        * accessible from the child which the (then) parent writes to for the
+        * child to read in the next iteration.
+        */
+        else if(fork_status == 0)
+        {
+            close(fd[1]);            
+            read(fd[0], &arr, sizeof(arr));
+            close(fd[0]);
+            
+            printf("\nThe process %d was spawned by parent process %d\n", (int)getpid(), (int)getppid());
+            arr_size = filter_print(arr, arr_size, i);
+            
+            int pipe_status = pipe(fd);
+            fork_status = fork();
+            if(fork_status > 0)
+            {
+                close(fd[0]);
+                write(fd[1], &arr, sizeof(arr));
+                close(fd[1]);
+                wait(0);
+            }
+        }
+        else
+        {
+            printf("\nChild creation unsuccessful\n");
+        }
     }
         
-    // The final child process needs to read from the pipe, filter and print the array
+    /**
+     *  The final child process needs to read from the pipe, filter and print the array.
+     */
     if ((fork_status == 0) && (i == children))
     {
+        close(fd[1]);
         int rd = read(fd[0], &arr, sizeof(arr));
         close(fd[0]);
         printf("\nThe process %d was spawned by parent process %d\n", (int)getpid(), (int)getppid());            
-        n = filter_print(arr, n, i);
+        n = filter_print(arr, arr_size, i);
+        _exit(0);
     }   
 
     free(arr);
