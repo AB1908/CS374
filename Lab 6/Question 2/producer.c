0528f366f112c9a1fdde67352ace94d76cc77562
AB1908 <abhishekbhattacherjee@live.com>
2018-09-04 19:22:24 +0530

    Rewrite input method and add error handling

--- Lab 6/Question 2/producer.c
+++ Lab 6/Question 2/producer.c
@@ -1,22 +1,31 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int main()
 {
-        char buf[100];
+	int errno;
+	char *buf = NULL;
+	size_t size = 0;
+	// FILE *stream;
 
-	if (mkfifo("messages.txt", 0666) == -1)	{
-		printf("\nFIFO creation was unsuccesful.\n");
-	}
+	if (mkfifo("messages.txt", 0666) == -1)
+		if (errno = EEXIST)
+			printf("\nProducer: %s\n", strerror(errno));
 
-        printf("\nEnter message to pass to consumer:");
-        scanf("%s", buf);
+	printf("\nEnter message to pass to consumer:");
+	getline(&buf, &size, stdin);
 	int fd = open("messages.txt", O_WRONLY);
-	write(fd, buf, sizeof(buf));
-        close(fd);
-    
+	write(fd, buf, size);
+	close(fd);
+	
+	free(buf);
 	return 0;
 }
