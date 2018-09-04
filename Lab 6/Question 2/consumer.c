c9436d5ac487b170e4bbe8a8aee4f2f6bc8a1f58
AB1908 <abhishekbhattacherjee@live.com>
2018-09-04 19:24:46 +0530

    Remove commented out code

--- Lab 6/Question 2/consumer.c
+++ Lab 6/Question 2/consumer.c
@@ -8,14 +8,9 @@ int main()
 {
         char buf[100];
 
-	// int fifo_status = mkfifo("messages.txt", 0666);
-        // if (fifo_status == -1) {
-	// 	printf("\nFIFO creation was unsuccesful.\n");
-	// }
-
 	int fd = open("messages.txt", O_RDONLY);
 	read(fd, buf, 100);
-        printf("\nThe following message was received:%s", buf);
+        printf("\nThe following message was received by consumer:%s", buf);
         close(fd);
     
 	return 0;
