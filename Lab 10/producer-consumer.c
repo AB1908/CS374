49e81f9b9e25fa8572710cd27dda04754d3d45d5
AB1908 <abhishekbhattacherjee@live.com>
2018-10-14 19:34:19 +0000

    Refactor implementation for better readability
    - A separate section was created for critical data as there was a
    needless duplication of pointers. Item counts were left out as they
    logically do not fit into the critical_data struct. This also removed
    the overhead of repeatedly assigning the pointer addresses to the
    thread_data structs, which has been replaced by an address assignment to
    the critical_data struct.
    - Buffer operations were split to resolve function dependencies and to
    enhance readability.
    - Missing return statements were added.
    - Initialisations were moved to the top of main() to comply with ANSI C
    Style.

--- Lab 10/producer-consumer.c
+++ Lab 10/producer-consumer.c
@@ -5,34 +5,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct thread_data {
-	pthread_t pthread_no;
+#define produce 1
+#define consume 0
+
+struct critical_data {
 	int *buffer;
-	int thread_id;
-	int *buffer_size;
-	int *buffer_read;
-	int *buffer_write;
-	int is_producer;
-	int *item_read_count;
-	int *item_write_count;
+	int buffer_size;
+	int read_index;
+	int write_index;
 	sem_t *free_buffer;
 	sem_t *loaded_buffer;
 	sem_t *buffer_sem;
 };
 
+struct thread_data {
+	pthread_t pthread_no;
+	int thread_id;
+	int is_producer;
+	int *item_read_count;
+	int *item_write_count;
+	struct critical_data *crt_data;
+};
+
+int delete_buffer_item(struct critical_data *cd)
+{
+	cd->buffer[cd->read_index] = 0;
+	cd->read_index = --(cd->read_index) % cd->buffer_size;
+	return 0;
+}
+
+int write_buffer_item(struct critical_data *cd, int item)
+{
+	cd->buffer[cd->write_index] = item;
+	cd->write_index = ++(cd->write_index) % cd->buffer_size;
+	return 0;
+}
+
 int get_buffer_item(struct thread_data *td)
 {
-	printf("\nItem %d was read from the buffer by Thread %d.\n", ++(*td->item_read_count), td->thread_id);
-	td->buffer[*td->buffer_read] = 0;
-	*td->buffer_read = --(*td->buffer_read) % *td->buffer_size;
+	printf("\nItem %d was read from the buffer by Thread %d.\n"
+		, ++(*td->item_read_count), td->thread_id);
+	delete_buffer_item(td->crt_data);
 	return 0;
 }
 
 int put_buffer_item(struct thread_data *td)
 {
-	printf("\nItem %d was put in the buffer by Thread %d.\n", ++(*td->item_write_count), td->thread_id);
-	td->buffer[*td->buffer_write] = *td->item_write_count;
-	*td->buffer_write = ++(*td->buffer_write) % *td->buffer_size;
+	printf("\nItem %d was put in the buffer by Thread %d.\n"
+		, ++(*td->item_write_count), td->thread_id);
+	write_buffer_item(td->crt_data, *td->item_write_count);
+	return 0;
+}
+
+int buffer_operation(struct thread_data *td, int prod)
+{
+	sem_wait(td->crt_data->buffer_sem);
+	if (prod)
+		put_buffer_item(td);
+	else
+		get_buffer_item(td);
+	sem_post(td->crt_data->buffer_sem);
 	return 0;
 }
 
@@ -41,12 +73,11 @@ int producer(struct thread_data *td)
 	int i;
 	for (i = 0; i < 4; i++)
 	{
-		sem_wait(td->free_buffer);
-		sem_wait(td->buffer_sem);
-		put_buffer_item(td);
-		sem_post(td->buffer_sem);
-		sem_post(td->loaded_buffer);
+		sem_wait(td->crt_data->free_buffer);
+		buffer_operation(td, produce);
+		sem_post(td->crt_data->loaded_buffer);
 	}
+	return 0;
 }
 
 int consumer(struct thread_data *td)
@@ -54,12 +85,11 @@ int consumer(struct thread_data *td)
 	int i;
 	for (i = 0; i < 4; i++)
 	{
-		sem_wait(td->loaded_buffer);
-		sem_wait(td->buffer_sem);
-		get_buffer_item(td);
-		sem_post(td->buffer_sem);
-		sem_post(td->free_buffer);
+		sem_wait(td->crt_data->loaded_buffer);
+		buffer_operation(td, consume);
+		sem_post(td->crt_data->free_buffer);
 	}
+	return 0;
 }
 
 void *thread_start(void *arg)
@@ -70,42 +100,46 @@ void *thread_start(void *arg)
 		producer(td);
 	else
 		consumer(td);
+	return NULL;
 }
 
 int main()
 {
 	int producer_number, consumer_number;
 	int bufsize;
+	int i;
+	int *buf; 
+	int rdcnt = 0;
+	int wrtcnt = 0;
+	struct critical_data *cdata;
+	sem_t fillbuf, emptybuf, buflock;
+	
 	printf("\nEnter number of producer threads to be created:");
 	scanf("%d", &producer_number);
 	printf("\nEnter number of consumer threads to be created:");
 	scanf("%d", &consumer_number);
 	printf("\nEnter size of buffer:");
 	scanf("%d", &bufsize);
-	int i;
 
-	int *buf = malloc(bufsize * sizeof(int));
-	buf = memset(buf, 0, bufsize);
-	int bufrd = 0;
-	int bufwr = 0;
 	struct thread_data *thread_count[producer_number + consumer_number];
-	sem_t fillbuf, emptybuf, buflock;
+	buf = malloc(bufsize * sizeof(int));
+	buf = memset(buf, 0, bufsize);
 	sem_init(&buflock, 0, 1);
 	sem_init(&fillbuf, 0, 0);
 	sem_init(&emptybuf, 0, bufsize);
-	int rdcnt = 0;
-	int wrtcnt = 0;
+	cdata = malloc(sizeof(struct critical_data));
+	cdata->buffer = buf; 
+	cdata->read_index = 0;
+	cdata->write_index = 0;
+	cdata->buffer_size = bufsize;
+	cdata->free_buffer = &emptybuf;
+	cdata->loaded_buffer = &fillbuf;
+	cdata->buffer_sem = &buflock;
 
 	for (i = 0; i < producer_number + consumer_number; i++) {
 		thread_count[i] = malloc(sizeof(struct thread_data));
-		thread_count[i]->buffer = buf; 
-		thread_count[i]->buffer_read = &bufrd;
-		thread_count[i]->buffer_write = &bufwr;
-		thread_count[i]->buffer_size = &bufsize;
+		thread_count[i]->crt_data = cdata;
 		thread_count[i]->thread_id = i;
-		thread_count[i]->free_buffer = &emptybuf;
-		thread_count[i]->loaded_buffer = &fillbuf;
-		thread_count[i]->buffer_sem = &buflock;
 		thread_count[i]->is_producer = i < producer_number ? 1 : 0;
 		thread_count[i]->item_read_count = &rdcnt;
 		thread_count[i]->item_write_count = &wrtcnt;
@@ -114,4 +148,5 @@ int main()
 	for (i = 0; i< producer_number + consumer_number; i++) {
 		pthread_join(thread_count[i]->pthread_no, NULL);
 	}
+	return 0;
 }
