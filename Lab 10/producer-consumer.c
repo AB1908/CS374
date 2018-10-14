25cbe452b7092d618e524ae248a1fd64c543ae23
AB1908 <abhishekbhattacherjee@live.com>
2018-10-14 17:22:31 +0000

    Change buffer implementation
    - The buffer was initially implemented as a stack although a queue was
    expected. This was changed by using the array as a queue by introducing
    pointers which kept track of the front and end.
    - Two separate pointers were required to keep track of the read and
    write item counts as the usage of multiple threads would require that
    they be tracked separately.
    - Unnecessary comments were removed and the array allocation was fixed.
    Array initalisation was also added using memset.

--- Lab 10/producer-consumer.c
+++ Lab 10/producer-consumer.c
@@ -3,17 +3,18 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-#define pft printf("\nNo errors till now\n")
-// #define pf() printf("")
+#include <string.h>
 
 struct thread_data {
 	pthread_t pthread_no;
 	int *buffer;
 	int thread_id;
-	int *buffer_end;
 	int *buffer_size;
+	int *buffer_read;
+	int *buffer_write;
 	int is_producer;
+	int *item_read_count;
+	int *item_write_count;
 	sem_t *free_buffer;
 	sem_t *loaded_buffer;
 	sem_t *buffer_sem;
@@ -21,21 +22,17 @@ struct thread_data {
 
 int get_buffer_item(struct thread_data *td)
 {
-	// pft;
-	printf("\nItem %d was read from the buffer by Thread %d.\n", *td->buffer_end + 1, td->thread_id);
-	// printf("\nbufend:%d\n", *td->buffer_end);
-	td->buffer[*td->buffer_end]--;
-	(*td->buffer_end)--;
+	printf("\nItem %d was read from the buffer by Thread %d.\n", ++(*td->item_read_count), td->thread_id);
+	td->buffer[*td->buffer_read] = 0;
+	*td->buffer_read = --(*td->buffer_read) % *td->buffer_size;
 	return 0;
 }
 
 int put_buffer_item(struct thread_data *td)
 {
-	// pft;
-	printf("\nItem %d was put in the buffer by Thread %d.\n", *td->buffer_end + 1, td->thread_id);
-	// printf("\nbufend:%d\n", *td->buffer_end);
-	td->buffer[*td->buffer_end]++;
-	(*td->buffer_end)++;
+	printf("\nItem %d was put in the buffer by Thread %d.\n", ++(*td->item_write_count), td->thread_id);
+	td->buffer[*td->buffer_write] = *td->item_write_count;
+	*td->buffer_write = ++(*td->buffer_write) % *td->buffer_size;
 	return 0;
 }
 
@@ -44,7 +41,6 @@ int producer(struct thread_data *td)
 	int i;
 	for (i = 0; i < 4; i++)
 	{
-	// pft;
 		sem_wait(td->free_buffer);
 		sem_wait(td->buffer_sem);
 		put_buffer_item(td);
@@ -88,23 +84,31 @@ int main()
 	scanf("%d", &bufsize);
 	int i;
 
-	int buf[bufsize];
-	int bufend = 0;
+	int *buf = malloc(bufsize * sizeof(int));
+	buf = memset(buf, 0, bufsize);
+	int bufrd = 0;
+	int bufwr = 0;
 	struct thread_data *thread_count[producer_number + consumer_number];
 	sem_t fillbuf, emptybuf, buflock;
 	sem_init(&buflock, 0, 1);
 	sem_init(&fillbuf, 0, 0);
 	sem_init(&emptybuf, 0, bufsize);
+	int rdcnt = 0;
+	int wrtcnt = 0;
+
 	for (i = 0; i < producer_number + consumer_number; i++) {
 		thread_count[i] = malloc(sizeof(struct thread_data));
 		thread_count[i]->buffer = buf; 
-		thread_count[i]->buffer_end = &bufend;
+		thread_count[i]->buffer_read = &bufrd;
+		thread_count[i]->buffer_write = &bufwr;
 		thread_count[i]->buffer_size = &bufsize;
 		thread_count[i]->thread_id = i;
 		thread_count[i]->free_buffer = &emptybuf;
 		thread_count[i]->loaded_buffer = &fillbuf;
 		thread_count[i]->buffer_sem = &buflock;
 		thread_count[i]->is_producer = i < producer_number ? 1 : 0;
+		thread_count[i]->item_read_count = &rdcnt;
+		thread_count[i]->item_write_count = &wrtcnt;
 		pthread_create(&thread_count[i]->pthread_no, NULL, &thread_start, (void *) thread_count[i]);
 	}
 	for (i = 0; i< producer_number + consumer_number; i++) {
